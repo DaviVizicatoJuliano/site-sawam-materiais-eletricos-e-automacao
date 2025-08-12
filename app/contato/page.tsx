8d85d704181d1d850719a439048a21e816261c02
Davi Vizicato Juliano <davi1234.juliano@gmail.com>
2025-08-12 15:18:02 -0300

    Update page.tsx

--- app/contato/page.tsx
+++ app/contato/page.tsx
@@ -63,29 +63,9 @@ export default function ContactPage() {
       </div>
 
       {/* Google Map */}
-     <div className="mt-8 grid grid-cols-1 md:grid-cols-2 gap-4">
+     <div className="mt-8">
         <h2 className="text-2xl font-bold mb-6 col-span-2">Nossa Localização</h2>
-        
-        {/* Antigo Mapa */}
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3679.8639165976797!2d-47.35256708928871!3d-22.733298879291628!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94c89bbe166e412f%3A0x11cffd91428cee9d!2sSawam%20Materiais%20El%C3%A9tricos%20e%20Automa%C3%A7%C3%A3o%20industrial%20LTDA-ME!5e0!3m2!1spt-PT!2sbr!4v1744071046503!5m2!1spt-PT!2sbr" 
-          width="100%" 
-          height="450" 
-           style={{ border: 0 }} 
-          allowfullscreen="" 
-          loading="lazy" 
-          referrerpolicy="no-referrer-when-downgrade">
-        </iframe>
-    
-        {/* Novo Mapa */}
-        <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d99157.1855552122!2d-47.43748500714959!3d-22.543509904179466!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94c89a2e18c9b78f%3A0x64128060bbd5a5e4!2sSawam!5e0!3m2!1spt-PT!2sbr!4v1744069218913!5m2!1spt-PT!2sbr"
-          width="100%"
-          height="450"
-          style={{ border: 0 }}
-          allowFullScreen=""
-          loading="lazy"
-          referrerPolicy="no-referrer-when-downgrade"
-        ></iframe>
+        <GoogleMap />
       </div>
     </div>
   )
