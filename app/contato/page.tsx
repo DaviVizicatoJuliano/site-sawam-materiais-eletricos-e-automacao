f9b16dee89c1f1186ddd92bc963729e65528b017
Davi Vizicato Juliano <davi1234.juliano@gmail.com>
2025-08-12 15:25:31 -0300

    Update page.tsx

--- app/contato/page.tsx
+++ app/contato/page.tsx
@@ -1,8 +1,4 @@
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import { Textarea } from "@/components/ui/textarea"
 import { Mail, MapPin, Phone } from "lucide-react"
-import GoogleMap from "@/components/google-map"
 
 export default function ContactPage() {
   return (
@@ -62,10 +58,29 @@ export default function ContactPage() {
         </div>
       </div>
 
-      {/* Google Map */}
-     <div className="mt-8">
-        <h2 className="text-2xl font-bold mb-6 col-span-2">Nossa Localização</h2>
-        <GoogleMap />
+      {/* Google Maps */}
+      <div className="mt-8">
+        <h2 className="text-2xl font-bold mb-6">Nossas Localizações</h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3679.8639165976797!2d-47.35256708928871!3d-22.733298879291628!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94c89bbe166e412f%3A0x11cffd91428cee9d!2sSawam%20Materiais%20El%C3%A9tricos%20e%20Automa%C3%A7%C3%A3o%20industrial%20LTDA-ME!5e0!3m2!1spt-PT!2sbr!4v1744071046503!5m2!12"
+            width="100%"
+            height="450"
+            style={{ border: 0 }}
+            allowFullScreen
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+          ></iframe>
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3679.8639165976797!2d-47.35256708928871!3d-22.733298879291628!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94c89bbe166e412f%3A0x11cffd91428cee9d!2sSawam%20Materiais%20El%C3%A9tricos%20e%20Automa%C3%A7%C3%A3o%20industrial%20LTDA-ME!5e0!3m2!1spt-PT!2sbr!4v1744071046503!5m2!13"
+            width="100%"
+            height="450"
+            style={{ border: 0 }}
+            allowFullScreen
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+          ></iframe>
+        </div>
       </div>
     </div>
   )
